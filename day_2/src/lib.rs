f6b94ed6f9788125f727c48449de2e47529144e1
coutlaw <cass.d.outlaw@gmail.com>
2020-12-02 11:56:52 -0500

    day 2, advent of code, part 1

--- day_2/src/lib.rs
+++ day_2/src/lib.rs
@@ -3,58 +3,70 @@ use std::fs;
 use std::env;
 
 pub struct Config {
-	pub query: String,
 	pub filename: String,
-	pub case_sensitive: bool,
 }
 
 impl Config {
 	pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
 		// throw away first arg since its the program name
-			args.next();
-
-			// query string from Iterator
-			let query = match args.next() {
-				Some(arg) => arg,
-				None => return Err("Didn't get a query string"),
-			};
+            args.next();
+            
 			// filename string form Iterator
 			let filename = match args.next() {
 				Some(arg) => arg,
 				None => return Err("Didn't get a filename"),
 			};
-
-			// case sensitive boolean
-			let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 	
-			Ok(Config { query, filename, case_sensitive })
+			Ok(Config { filename })
 	}
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 	let contents = fs::read_to_string(config.filename)?;
 
-	let results = if config.case_sensitive {
-		search(&config.query, &contents)
-	} else {
-		search_case_insensitive(&config.query, &contents)
-	};
+	let results = search(&contents);
 
-	for line in results {
-		println!("{}", line);
-	}
+	println!("{}", results);
 
 	Ok(())
 }
 
 // the lifetime is connected to the return value, since its a slice over contents
 // we need the vector to live as long as the contents its a reference too!
-fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	contents.lines().filter(|line| line.contains(query)).collect()
-}
+fn search<'a>(contents: &'a str) -> i32 {
+    let mut count = 0;
 
-fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	contents.lines().filter(|line| line.to_lowercase().contains(&query.to_lowercase())).collect()
+    for line in contents.lines() {
+        // THIS IS GOING TO GET SHADY, its a quick and dirty solution plz don't judge
+        // TODO: make this not be so bad
+        let mut iter = line.split_whitespace();
+        if let Some(i) = iter.next() {
+            let mut numbs = i.split("-");
+            let min = numbs.next().unwrap().parse::<usize>().unwrap();
+            let max = numbs.next().unwrap().parse::<usize>().unwrap();
+            if let Some(i) = iter.next() {
+                let ch = i.chars().nth(0).unwrap();
+                if let Some(i) = iter.next() {
+                    let password = i;
+                    let password_values = password.matches(ch).count();
+        
+                    if password_values >= min && password_values <= max {
+                        count += 1;
+                    }
+                } else {
+                    panic!("ahhhh");
+                }
+            } else {
+                panic!("ahhhh");
+            }
+        } else {
+            panic!("ahhhh");
+        }
+
+        
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -63,31 +75,15 @@ mod tests {
 
 	#[test]
 	fn case_sensitive() {
-		let query = "duct";
 		// over to the left to remove tabs from the string
 		let contents = "\
-Rust: 
-safe, fast, productive.
-Pick three.
-Duct tape.";
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+";
 // should fail to find duct tape because its case sensitive
 
-		assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+		assert_eq!(2, search(contents));
 
-	}
-
-	#[test]
-	fn case_insensitive() {
-		let query = "rUsT";
-		let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-		assert_eq!(
-			vec!["Rust:", "Trust me."],
-			search_case_insensitive(query, contents)
-		);
 	}
 }
